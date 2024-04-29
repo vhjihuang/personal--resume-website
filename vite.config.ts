395b9e48a989c34fd91e4c606d4a2a768bf0750e
vhjihuang <1076891184@qq.com>
2024-04-29 15:24:02 +0800

    feat(vite.config.ts): 添加base属性

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './personal-resume-website',
+  base: '/personal-resume-website',
   plugins: [vue()],
   css: {
     postcss: {
